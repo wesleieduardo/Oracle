32540fda482bb825439858392ecb486eab57bbcf
wesleieduardo <81769371+wesleieduardo@users.noreply.github.com>
2021-04-05 11:18:32 -0300

    Add files via upload

---
+++ 5 - Remover marca dagua - Tabela.sql
@@ -0,0 +1,43 @@
+/*================================================================*/
+/* CURSOR PARA REMOVER MARCA D'AGUA DA TABELA, APÓS DELETAR       */
+/* REGISTROS DO BANCO E LIBERAR O ESPAÇO EM USO (LIMITE LÓGICO).  */
+/* APLICA-SE NO ORACLE EXPRESS EDTION                             */
+/*================================================================*/
+
+declare
+
+vcmd varchar(500);
+
+begin
+
+  for c1_row in (
+  
+                  SELECT owner,
+                         tablespace_name,
+                         segment_name,
+                         segment_type,
+                         round(sum(bytes / 1024 / 1024), 2) as Tamanho_MB
+                   FROM dba_segments
+                  WHERE owner = 'LENKE'
+                    AND segment_type = 'TABLE'
+                  GROUP BY owner, tablespace_name, segment_name, segment_type
+                  order by tamanho_mb desc
+                
+                 
+                 ) LOOP
+    BEGIN
+    
+    execute immediate 'ALTER TABLE ' || C1_ROW.SEGMENT_NAME || ' ENABLE ROW MOVEMENT';
+    execute immediate 'ALTER TABLE ' || C1_ROW.SEGMENT_NAME || ' shrink space compact';  
+    execute immediate 'ALTER TABLE ' || C1_ROW.SEGMENT_NAME || ' shrink space';   
+                       
+    EXCEPTION
+    WHEN OTHERS then
+         DBMS_OUTPUT.put_line(sqlerrm);
+    END;                      
+  
+  END LOOP;
+
+END;
+
+/*================================================================*/
